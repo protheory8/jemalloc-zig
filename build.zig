914507fa6f39025c5743f2d5a0b12589d3ae8d48
protheory8 <editaw5@gmail.com>
2020-08-02 15:12:04 +0000

    Move all tests into src/tests.zig

--- build.zig
+++ build.zig
@@ -2,16 +2,17 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+
     const lib = b.addStaticLibrary("jemalloc-zig", "src/main.zig");
-    lib.addSystemIncludeDir("./src/c_include");
     lib.linkLibC();
+    lib.addSystemIncludeDir("./src/c_include");
     lib.linkSystemLibrary("jemalloc");
     lib.setBuildMode(mode);
     lib.install();
 
-    var main_tests = b.addTest("src/main.zig");
-    main_tests.addSystemIncludeDir("./src/c_include");
+    const main_tests = b.addTest("src/tests.zig"); 
     main_tests.linkLibC();
+    main_tests.addSystemIncludeDir("./src/c_include");
     main_tests.linkSystemLibrary("jemalloc");
     main_tests.setBuildMode(mode);
 
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,4 @@
 const std = @import("std");
-const testing = std.testing;
-const debug = std.debug;
 const mem = std.mem;
 const Allocator = mem.Allocator;
 const c = @cImport(@cInclude("jemalloc_zig_macro_glue.h"));
@@ -24,7 +22,6 @@ fn jemallocAllocFn(self: *Allocator, len: usize, ptr_align: u29, len_align: u29)
     }
 
     const full_len = c.malloc_usable_size(ptr);
-    debug.assert(full_len >= len);
     return ptr[0..mem.alignBackwardAnyAlign(full_len, len_align)];
 }
 
@@ -44,99 +41,4 @@ fn jemallocResizeFn(self: *Allocator, buf: []u8, new_len: usize, len_align: u29)
     }
 
     return error.OutOfMemory;
-}
-
-test "allocate memory and free" {
-    const TestStruct = struct {
-        title: []const u8,
-        width: i32,
-        height: i32,
-        index: usize,
-    };
-    
-    const memory: []TestStruct = jemalloc_allocator.alloc(TestStruct, 1) catch @panic("test failure");
-    jemalloc_allocator.free(memory);
-}
-
-test "allocate memory, use it and free" {
-    const TestStruct = struct {
-        width: i32,
-        height: i32,
-        title: []const u8,
-        index: usize,
-    };
-    
-    const memory: []TestStruct = jemalloc_allocator.alloc(TestStruct, 1) catch @panic("test failure");
-
-    memory[0] = TestStruct{
-        .width = 1280,
-        .height = 720,
-        .title = "Should work fine!",
-        .index = 12,
-    };
-
-    testing.expect(memory[0].width == 1280);
-    testing.expect(memory[0].height == 720);
-    testing.expect(memory[0].index == 12);
-
-    jemalloc_allocator.free(memory);
-}
-
-test "allocate memory, use it, reallocate memory, use it and free" {
-    const TestStruct = struct {
-        width: i32,
-        height: i32,
-        title: []const u8,
-        index: usize,
-    };
-
-    var memory: []TestStruct = jemalloc_allocator.alloc(TestStruct, 2) catch @panic("test failure");
-
-    memory[0] = TestStruct{
-        .width = 1280,
-        .height = 720,
-        .title = "Should work fine!",
-        .index = 12,
-    };
-
-    memory[1] = TestStruct{
-        .width = 1230,
-        .height = 820,
-        .title = "Cool game",
-        .index = 120,
-    };
-
-    memory = jemalloc_allocator.realloc(memory, 4) catch @panic("test failure");
-
-    memory[2] = TestStruct{
-        .width = 12612,
-        .height = 45,
-        .title = "Window title",
-        .index = 31,
-    };
-
-    memory[3] = TestStruct{
-        .width = 125,
-        .height = 1,
-        .title = "Game title",
-        .index = 21,
-    };
-
-    testing.expect(memory[0].width == 1280);
-    testing.expect(memory[0].height == 720);
-    testing.expect(memory[0].index == 12);
-
-    testing.expect(memory[1].width == 1230);
-    testing.expect(memory[1].height == 820);
-    testing.expect(memory[1].index == 120);
-
-    testing.expect(memory[2].width == 12612);
-    testing.expect(memory[2].height == 45);
-    testing.expect(memory[2].index == 31);
-
-    testing.expect(memory[3].width == 125);
-    testing.expect(memory[3].height == 1);
-    testing.expect(memory[3].index == 21);
-
-    jemalloc_allocator.free(memory);
 }
